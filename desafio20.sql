46919cafb92e303731ff1298073467a5b9c9be74
Caio <caiusito@gmail.com>
2022-10-20 00:11:03 -0300

    Requisito 20 - melhorando a qualidade

--- desafio20.sql
+++ desafio20.sql
@@ -1,1 +1,2 @@
-INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, inventory_id) VALUE (69, 80, 15000, 15000, 0, 2, NULL, NULL, 129);
+INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, inventory_id) 
+VALUE (69, 80, 15.000, 15.000, 0, 2, 129);
